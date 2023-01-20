011e042f6e30adb2f0a27d8fede47e8ffb51d578
VSantiM <63769445+VSantiM@users.noreply.github.com>
2023-01-19 22:45:12 -0300

    Projeto Xadrez - Exceptions

---
+++ ProjetoXadrez/src/chess/ChessPiece.java
@@ -0,0 +1,19 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+    private Color color;
+    private Board board;
+
+
+    public ChessPiece(Board board, Color color) {
+        super(board);
+        this.board = board;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
