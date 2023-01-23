a1de9555d9b3397376decbd1948444f9180eeb42
VSantiM <63769445+VSantiM@users.noreply.github.com>
2023-01-22 21:14:18 -0300

    Projeto Xadrez - Move Pieces methods

--- ProjetoXadrez/src/boardgame/Board.java
+++ ProjetoXadrez/src/boardgame/Board.java
@@ -44,6 +44,16 @@ public class Board {
         piece.position = position;
     }
 
+    public Piece removePiece(Position position) {
+        if (!positionExists(position)) throw new BoardException("ERROR: Position does not exist");
+        if (piece(position) == null) return null;
+
+        Piece tmpPiece = piece(position);
+        tmpPiece.position = null;
+        pieces[position.getRow()][position.getColumn()] = null;
+        return tmpPiece;
+    }
+
     public boolean positionExists(int row, int column) {
         return row >= 0 && row < rows && column >= 0 && column < columns;
     }
